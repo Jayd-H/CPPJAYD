7702b483bd5871829e1264a169a0d92ea7950c81
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-02-26 03:11:49 +0000

    feat: functionality to only conditionally launch vs code

--- cppjayd.py
+++ cppjayd.py
@@ -5,7 +5,7 @@ import subprocess
 import json
 
 
-def create_project(project_name, bits):
+def create_project(project_name, bits, open_vscode):
     current_dir = os.path.dirname(os.path.realpath(__file__))
     project_dir = os.path.join(os.getcwd(), project_name)
     vscode_dir = os.path.join(project_dir, ".vscode")
@@ -26,6 +26,7 @@ def create_project(project_name, bits):
 
     shutil.copy(tasks_template_path, vscode_dir)
     shutil.copy(launch_template_path, vscode_dir)
+
     # Modify tasks.json if necessary based on the bits argument
     tasks_json_path = os.path.join(vscode_dir, "tasks.json")
     with open(tasks_json_path, "r+") as tasks_file:
@@ -34,8 +35,9 @@ def create_project(project_name, bits):
         json.dump(tasks_content, tasks_file, indent=4)
         tasks_file.truncate()
 
-    # Launch VS Code with the new project directory
-    subprocess.run(["code .", project_dir])
+    # Conditionally launch VS Code with the new project directory
+    if open_vscode:
+        subprocess.run(["code", project_dir])
 
 
 if __name__ == "__main__":
@@ -46,6 +48,9 @@ if __name__ == "__main__":
         "bits", choices=["32", "64"], help="The bit version for the project (32 or 64)."
     )
     parser.add_argument("projectname", help="The name of the project.")
+    parser.add_argument(
+        "--vs", help="Open the project in Visual Studio Code.", action="store_true"
+    )
     args = parser.parse_args()
 
-    create_project(args.projectname, args.bits)
+    create_project(args.projectname, args.bits, args.vs)
